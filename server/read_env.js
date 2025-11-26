f6a50d76cc91e9956ed9e091d19faa53955b6b0f
hmvtechs <hmvtechs@gmail.com>
2025-11-26 12:09:41 -0300

    fix git

--- server/read_env.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('.env', 'utf8');
-console.log(content);
---
+++ server/src/types/global.d.ts
@@ -0,0 +1,8 @@
+export { };
+
+declare global {
+    interface Error {
+        code?: string;
+        status?: number;
+    }
+}
